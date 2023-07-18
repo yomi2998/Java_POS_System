4ca3abea0a7fddd57121876fdb467c9e53473809
ishwar1810 <92344666+ishwar1810@users.noreply.github.com>
2023-07-18 13:42:42 +0800

    Update Java_POS_System.java

--- src/java_pos_system/Java_POS_System.java
+++ src/java_pos_system/Java_POS_System.java
@@ -1,7 +1,7 @@
 package java_pos_system;
 public class Java_POS_System {
     public static void main(String[] args) {
-        // TODO code application logic here (hehehehe)
+        // TODO code application logic here
     }
     
 }
