6ccd2fee2c9fb8e737a5eeefabea3c19217c4fe5
yomi2998 <siewjh-wm22@student.tarc.edu.my>
2023-07-19 18:14:40 +0800

    code intergration

--- src/java_pos_system/GUI.java
+++ src/java_pos_system/GUI.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package java_pos_system;
 import javax.swing.*;
 import java.awt.*;
-/**
- *
- * @author khshu
- */
 public class GUI {
     
 
-    public static void main(String args[]){
+    public void gui_start(){
         //Creating the Frame
         JFrame frame = new JFrame("Welcome to ZZZTOJ Accessories");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/java_pos_system/proc_main.java
+++ src/java_pos_system/proc_main.java
@@ -1,7 +1,8 @@
 package java_pos_system;
 public class proc_main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        GUI gui = new GUI();
+        gui.gui_start();
     }
     
 }
