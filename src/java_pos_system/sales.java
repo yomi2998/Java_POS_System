ee93669a3cb8622a86570af3a6612b7cef40463f
yomi2998 <siewjh-wm22@student.tarc.edu.my>
2023-07-21 00:44:36 +0800

    Update sales.java

--- src/java_pos_system/sales.java
+++ src/java_pos_system/sales.java
@@ -1,8 +1,6 @@
 package java_pos_system;
-
-import java.util.ArrayDeque;
 import java.util.Deque;
-
+import java.time.LocalDate;
 public class Sales {
     public enum paymentType {
         CASH,
@@ -13,5 +11,14 @@ public class Sales {
     private String customer;
     private Deque<Item> item;
     private Date salesDate;
+    private double totalPrice;
     private int paymentMethod;
+    Sales(String salesID, String customer) {
+        this.salesID = salesID;
+        this.customer = customer;
+        LocalDate now = LocalDate.now();
+        this.salesDate = new Date(now.getDayOfMonth(), now.getMonthValue(), now.getYear());
+        this.totalPrice = 0.0;
+        this.paymentMethod = -1;
+    }
 }
