e84d77c3f1df262e380c174f80958ada517e2223
yomi2998 <siewjh-wm22@student.tarc.edu.my>
2023-07-24 08:53:15 +0800

    Create Database.java

---
+++ src/java_pos_system/Database.java
@@ -0,0 +1,52 @@
+package java_pos_system;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+
+    private Connection conn;
+    private ResultSet result;
+
+    Database() throws SQLException {
+        this.conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/pos_system", "root", "");
+    }
+
+    public void runCommand(String cmd) throws SQLException {
+        Statement statement = conn.createStatement();
+        this.result = statement.executeQuery(cmd);
+    }
+
+    public List<String> getString(String col) throws SQLException {
+        List<String> str = new ArrayList<>();
+        while (this.result.next()) {
+            str.add(this.result.getString(col));
+        }
+        return str;
+    }
+
+    public List<Integer> getInt(String col) throws SQLException {
+        List<Integer> i = new ArrayList<>();
+        while (this.result.next()) {
+            i.add(this.result.getInt(col));
+        }
+        return i;
+    }
+
+    public List<Double> getDouble(String col) throws SQLException {
+        List<Double> d = new ArrayList<>();
+        while (this.result.next()) {
+            d.add(this.result.getDouble(col));
+        }
+        return d;
+    }
+
+    public List<Boolean> getBoolean(String col) throws SQLException {
+        List<Boolean> b = new ArrayList<>();
+        while (this.result.next()) {
+            b.add(this.result.getBoolean(col));
+        }
+        return b;
+    }
+}
