0d14be2fac6ff4afb45fae15983f57111556aaa0
yomi2998 <siewjh-wm22@student.tarc.edu.my>
2023-07-24 12:28:33 +0800

    Update item.java

--- src/java_pos_system/item.java
+++ src/java_pos_system/item.java
@@ -1,5 +1,8 @@
 package java_pos_system;
+
 public class Item {
+
+    Receiver receiver = new Receiver();
     private String itemID;
     private String itemName;
     private double itemPrice;
@@ -7,20 +10,83 @@ public class Item {
     private String itemSupplier;
     private int itemAvailable;
     private boolean forSale;
-    private Date date_imported;
+    private Date dateImported;
+
     Item(String itemID) {
         this.itemID = itemID;
     }
-    public void setItemDetails() {
-        Receiver receiver = new Receiver();
+
+    public String getItemID() {
+        return this.itemID;
+    }
+
+    public String getItemName() {
+        return this.itemName;
+    }
+
+    public void setItemName() {
         System.out.println("Setting item details for item " + this.itemID);
         this.itemName = receiver.getStr("Enter item name: ", 3, 50);
+        System.out.println("Details for item " + this.itemID + " has been set!");
+    }
+
+    public double getItemPrice() {
+        return this.itemPrice;
+    }
+
+    public void setItemPrice() {
+        System.out.println("Setting item details for item " + this.itemID);
         this.itemPrice = receiver.getDouble("Enter price for the item: ", 0.0, 999999999.0);
+        System.out.println("Details for item " + this.itemID + " has been set!");
+    }
+
+    public String getItemDescription() {
+        return this.itemDescription;
+    }
+
+    public void setItemDescription() {
+        System.out.println("Setting item details for item " + this.itemID);
         this.itemDescription = receiver.getStr("Enter item description: ", 0, 99999);
+        System.out.println("Details for item " + this.itemID + " has been set!");
+    }
+
+    public String getItemSupplier() {
+        return this.itemSupplier;
+    }
+
+    public void setItemSupplier() {
+        System.out.println("Setting item details for item " + this.itemID);
         this.itemSupplier = receiver.getStr("Enter supplier's name: ", 0, 100);
+        System.out.println("Details for item " + this.itemID + " has been set!");
+    }
+
+    public int getItemAmount() {
+        return this.itemAvailable;
+    }
+
+    public void setItemAmount() {
+        System.out.println("Setting item details for item " + this.itemID);
         this.itemAvailable = receiver.getInt("Enter amount of item available: ", 0, 999999);
+        System.out.println("Details for item " + this.itemID + " has been set!");
+    }
+
+    public boolean getForSale() {
+        return this.forSale;
+    }
+
+    public void setForSale() {
+        System.out.println("Setting item details for item " + this.itemID);
         this.forSale = receiver.getYN("Item for sale? (Y/N): ");
-        this.date_imported = receiver.getDate("Enter import date (dd/mm/yyyy): ");
+        System.out.println("Details for item " + this.itemID + " has been set!");
+    }
+
+    public Date getImportDate() {
+        return this.dateImported;
+    }
+
+    public void setImportDate() {
+        System.out.println("Setting item details for item " + this.itemID);
+        this.dateImported = receiver.getDate("Enter import date (dd/mm/yyyy): ");
         System.out.println("Details for item " + this.itemID + " has been set!");
     }
 }
