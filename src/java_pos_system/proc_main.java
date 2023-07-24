dbecb9a63adf79aa51540d3331476dd26b00bcf6
yomi2998 <siewjh-wm22@student.tarc.edu.my>
2023-07-24 11:37:11 +0800

    intergration

--- src/java_pos_system/proc_main.java
+++ src/java_pos_system/proc_main.java
@@ -1,8 +1,13 @@
 package java_pos_system;
 public class proc_main {
+    static Account account = new Account();
+
+    public static void startSession() {
+        LoginPage loginPage = new LoginPage(account.getLoginInfo());
+    }
+
     public static void main(String[] args) {
-        GUI gui = new GUI();
-        gui.gui_start();
+        startSession();
     }
     
 }
